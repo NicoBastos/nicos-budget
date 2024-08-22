2478b43f68caa6c3ae382fa2ad1a69bd0e6e2585
nickbastos <nickbastos4@gmail.com>
2024-08-22 16:04:46 -0500

    migrated to next.js ...

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nico's Budget",
+  description: "A website I made to create a monthly budget.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
